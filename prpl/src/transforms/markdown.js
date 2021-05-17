2a6c2bc6d40fe24cd12f611cce6c78f0feec2e0c
Ty Hopp <hopp.ty.c@gmail.com>
2021-05-17 22:57:17 +0800

    Fix markdown escaping code blocks

--- prpl/src/transforms/markdown.js
+++ prpl/src/transforms/markdown.js
@@ -1,17 +1,26 @@
 const fs = require('fs');
-const transform = require('marked');
+const marked = require('marked');
+
+// Override code block rendering
+const renderer = {
+  code(code, infostring) {
+    return `<pre class="language-${infostring}"><code class="language-${infostring}">${code}</code></pre>`;
+  }
+};
+
+marked.use({ renderer });
 
 /**
  * Transforms a markdown file to an HTML string.
  * @param {string} file A markdown file
  * @returns {string} An HTML string
  */
-const markdown = file => {
+const markdown = (file) => {
   const markdown = fs.readFileSync(file).toString();
-  const html = transform(markdown);
+  const html = marked(markdown);
   return html;
-}
+};
 
 module.exports = {
   markdown
-}
+};
