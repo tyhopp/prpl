a27d0ed4b578abf8fa30fea7297a4440bfdd0df6
Ty Hopp <hopp.ty.c@gmail.com>
2021-07-10 12:24:22 +0800

    chore: Fix bin resolution

--- packages/core/bin/prpl.js
+++ packages/core/bin/prpl.js
@@ -1,36 +1,5 @@
 #!/usr/bin/env node
 
-import { interpolate } from '../dist/interpolate/interpolate.js';
-import { server } from '../dist/server/server.js';
-import { clearDist } from '../dist/lib/clear-dist.js';
-import { log } from '../dist/lib/log.js';
+const { interpolate } = require('../dist');
 
-const commands = {
-  build: true,
-  serve: true,
-  clear: true
-};
-
-const command = process.argv.reduce((selectedArg, arg) => {
-  if (arg in commands) {
-    selectedArg = arg;
-  }
-  return selectedArg;
-}, '');
-
-switch (command) {
-  case 'build':
-    await interpolate();
-    break;
-  case 'serve':
-    await server();
-    break;
-  case 'clear':
-    await clearDist();
-    break;
-  default:
-    log.error(
-      'Invalid command. Valid commands are:',
-      Object.keys(commands).join(', ')
-    );
-}
+interpolate();
---
+++ packages/server/bin/server.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const { serve } = require('../dist');
+
+serve();
