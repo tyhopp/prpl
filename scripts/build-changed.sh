b75ab3eaa581777b01b1669cf763ddf6d8f92e14
Ty Hopp <hopp.ty.c@gmail.com>
2021-12-23 16:53:33 +0800

    chore: Adjust release:changed script

--- scripts/build-changed.sh
+++ scripts/build-changed.sh
@@ -2,6 +2,8 @@ npm run clear
 
 lerna changed | while read package; do
   npx rollup -c rollup-package.config.js --scope $package || exit 1
-done
 
-npx rollup -c rollup-client.config.js
+  if [[ "$package" = "@prpl/core" ]]; then
+    npx rollup -c rollup-client.config.js
+  fi
+done
