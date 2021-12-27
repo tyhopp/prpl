ee75c439faf28834bdb032f92c3d3f92971d7974
Ty Hopp <hopp.ty.c@gmail.com>
2021-12-27 12:22:11 +0800

    chore: Deno example

---
+++ examples/deno/scripts/build.js
@@ -0,0 +1,14 @@
+import { interpolate } from '@prpl/core';
+
+// Default options
+const options = {
+  noClientJS: false,
+  templateRegex: (key) => new RegExp(`\\[${key}\\]`, 'g'),
+  markedOptions: {}
+};
+
+async function build() {
+  await interpolate({ options });
+}
+
+build();
