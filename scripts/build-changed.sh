ee9633fc628576b3c95cc467680c848d9d8e5ae2
Ty Hopp <hopp.ty.c@gmail.com>
2022-02-27 13:30:54 +0800

    refactor: Bundling

--- scripts/build-changed.sh
+++
@@ -1,9 +0,0 @@
-npm run clear
-
-lerna changed | while read package; do
-  npx rollup -c rollup-package.config.js --scope $package || exit 1
-
-  if [[ "$package" = "@prpl/core" ]]; then
-    npx rollup -c rollup-client.config.js
-  fi
-done
