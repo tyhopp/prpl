c8771e6e0efb987600f602b94ce779bbb79a5fab
Ty Hopp <hopp.ty.c@gmail.com>
2022-03-04 00:14:18 +0800

    fix(server): Hard reload for all non html/css files

--- packages/server/src/socket.ts
+++ packages/server/src/socket.ts
@@ -14,7 +14,7 @@ if ('WebSocket' in window) {
         // Handle HTML files
         if (href === '/' || href?.endsWith('.html')) {
           return response?.text()?.then((html) => {
-            const { origin, pathname } = window?.location || {};
+            const { origin, pathname } = window.location || {};
 
             // Refresh the html cache
             sessionStorage?.setItem(`prpl-${origin}${href}`, html);
@@ -23,53 +23,23 @@ if ('WebSocket' in window) {
             if (pathname === href) {
               const pseudoAnchor = document?.createElement('a');
               pseudoAnchor.href = href;
-              document?.querySelector('main')?.appendChild(pseudoAnchor);
+              document.querySelector('main')?.appendChild(pseudoAnchor);
               pseudoAnchor?.click();
             }
           });
         }
 
-        // TODO - Dependency tree awareness for atomic replacement
         // Handle CSS
         if (href?.endsWith('.css')) {
-          const styleElements = document?.querySelectorAll('[rel="stylesheet"]');
-          styleElements?.forEach((styleElement) => {
+          const styleElements = document.querySelectorAll('[rel="stylesheet"]');
+          styleElements.forEach((styleElement) => {
             styleElement?.parentNode?.replaceChild(styleElement, styleElement);
           });
           return;
         }
 
-        // Resolve relative path
-        let relativeHref = href;
-        const hrefPath = href?.split('/');
-        const currentPath = window?.location?.pathname?.split('/');
-        if (hrefPath && currentPath?.length) {
-          currentPath?.forEach((path, index) => {
-            if (!path?.length) {
-              return;
-            }
-            if (path === hrefPath?.[index]) {
-              relativeHref = relativeHref?.replace(`/${hrefPath?.[index]}`, '');
-            }
-          });
-        }
-        relativeHref = relativeHref?.[0] === '/' ? relativeHref?.slice(1) : relativeHref;
-
-        // TODO - Dependency tree awareness for atomic replacement
-        // Handle JS and other resources
-        const elements = document?.querySelectorAll(
-          `[href*="${relativeHref}"], [src*="${relativeHref}"]`
-        );
-        elements?.forEach((element) => {
-          if (element?.tagName?.toLowerCase() === 'script') {
-            const pseudoAnchor = document?.createElement('a');
-            pseudoAnchor.href = window?.location?.href;
-            document?.querySelector('main')?.appendChild(pseudoAnchor);
-            pseudoAnchor?.click();
-            return;
-          }
-          element?.parentNode?.replaceChild(element, element);
-        });
+        // For everything else, keep it simple and refresh the page
+        window.location.reload();
       })
       .catch((error) => console.warn(`[PRPL] Failed to refresh resource '${href}'. Error:`, error));
   };
