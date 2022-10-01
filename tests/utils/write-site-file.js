b3718b80fc02a8168ccd5c9894d64f5df5abff56
tyhopp <hopp.ty.c@gmail.com>
2022-10-01 12:35:46 +0800

    chore: Normalize paths in test utils

--- tests/utils/write-site-file.js
+++ tests/utils/write-site-file.js
@@ -2,7 +2,7 @@ import { writeFile } from 'fs/promises';
 import { resolve } from 'path';
 
 async function writeSiteFile({ target, om }) {
-  const srcPath = resolve(`sites/${target}`);
+  const srcPath = resolve('sites', target);
   await writeFile(srcPath, om.toString());
 }
 
