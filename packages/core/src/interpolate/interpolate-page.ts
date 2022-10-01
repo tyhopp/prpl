2d7629d989c4fb564c556af5bc508500d13d5fbe
tyhopp <hopp.ty.c@gmail.com>
2022-10-01 12:11:33 +0800

    chore: Debug windows in CI

--- packages/core/src/interpolate/interpolate-page.ts
+++ packages/core/src/interpolate/interpolate-page.ts
@@ -85,6 +85,10 @@ async function interpolatePage(args: {
           src: contentFiles?.[p]?.src,
           srcRelativeFilePath: contentFiles?.[p]?.srcRelativeFilePath
         });
+
+        // TODO: Remove, this is temporary to debug Windows in CI
+        console.log(`Parsing metadata for ${contentFiles?.[p]?.srcRelativeFilePath}`, { metadata });
+
         metadata.body = await transformMarkdown({
           markdown: metadata?.body,
           options
