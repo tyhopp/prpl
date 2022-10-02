17fc6aa49f11a5c60bc5c5fbd504926ba01bbff4
tyhopp <hopp.ty.c@gmail.com>
2022-10-02 15:17:11 +0800

    chore: Update sites script

--- scripts/update-sites.sh
+++ scripts/update-sites.sh
@@ -2,7 +2,7 @@
 
 # Update docs site
 cd docs
-npm i @prpl/core@latest @prpl/plugin-code-highlight@latest @prpl/plugin-css-imports@latest @prpl/plugin-html-imports@latest @prpl/plugin-sitemap@latest && npm i -D @prpl/server@latest
+npm i --workspaces=false @prpl/core@latest @prpl/plugin-code-highlight@latest @prpl/plugin-css-imports@latest @prpl/plugin-html-imports@latest @prpl/plugin-sitemap@latest && npm i --workspaces=false -D @prpl/server@latest
 cd ..
 git add .
 git commit -m "chore(docs): Update PRPL deps"
@@ -13,7 +13,7 @@ examples=("basic" "esm" "commonjs" "deno")
 
 for example in ${examples[@]}; do
   cd "examples/${example}"
-  npm i @prpl/core@latest @prpl/server@latest
+  npm i --workspaces=false @prpl/core@latest @prpl/server@latest
   cd ../..
 done
 
