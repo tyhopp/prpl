632e2fd66c830ae0cd0a783cf74d0cbada512e9d
tyhopp <hopp.ty.c@gmail.com>
2022-10-02 16:59:03 +0800

    chore: Rewrite update sites script to work with latest npm

--- scripts/update-sites.sh
+++ scripts/update-sites.sh
@@ -1,21 +1,40 @@
-# Docs site --------------------------------------------------
+# Update package.json and package-lock.json files with latest PRPL deps.
+# Some workspace commands (e.g. install, update) are borked, so we have to do this manually.
 
-# Update docs site
-cd docs
-npm i @prpl/core@latest @prpl/plugin-code-highlight@latest @prpl/plugin-css-imports@latest @prpl/plugin-html-imports@latest @prpl/plugin-sitemap@latest && npm i --workspaces=false -D @prpl/server@latest
-cd ..
-git add .
-git commit -m "chore(docs): Update PRPL deps"
+core_latest=$(npm view "@prpl/core" version)
+server_latest=$(npm view "@prpl/server" version)
 
-# Example sites --------------------------------------------------
+# Update example site deps
+npm pkg set "dependencies[@prpl/core]"="^${core_latest}" --workspace=examples
+npm pkg set "devDependencies[@prpl/server]"="^${server_latest}" --workspace=examples
 
-examples=("basic" "esm" "commonjs" "deno")
+# Update docs deps
+npm pkg set "dependencies[@prpl/core]"="^${core_latest}" --workspace=docs
+npm pkg set "devDependencies[@prpl/server]"="^${server_latest}" --workspace=docs
 
+plugins=("core" "plugin-code-highlight" "plugin-css-imports" "plugin-html-imports" "plugin-sitemap")
+
+for plugin in ${plugins[@]}; do
+  latest=$(npm view "@prpl/${plugin}" version)
+  npm pkg set "dependencies[@prpl/${plugin}]"="^${latest}" --workspace=docs
+done
+
+# Recreate lockfiles, workspaces + lockfiles are broken in npm
+examples=("examples/basic" "examples/commonjs" "examples/deno" "examples/esm")
+
+# Recreate example lockfiles
 for example in ${examples[@]}; do
-  cd "examples/${example}"
-  npm i @prpl/core@latest @prpl/server@latest
+  cd $example
+  rm package-lock.json
+  npm install --workspaces=false
   cd ../..
 done
 
-git add .
-git commit -m "chore(examples): Update PRPL deps"
+# Recreate docs lockfile
+cd docs
+rm package-lock.json
+npm install --workspaces=false
+cd ../
+
+# Show if tree is borked or not
+npm ls
